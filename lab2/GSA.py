7201edbeee4443b8f21d773659edb4044187e22c
Lovro Rabuzin <lovro.rabuzin@fer.hr>
2020-11-30 20:05:01 +0100

    Generiranje LR(1) stavki za ep-NKA

--- lab2/GSA.py
+++ lab2/GSA.py
@@ -1,10 +1,12 @@
-from ItemManager import *
+from lab2.ItemManager import *
 import pickle
 import sys
 
 class SyntaxAnalyzerGenerator:
     def __init__(self):
         self.item_manager = None
+        self.syn = []
+        self.populate()
 
 
     def populate(self):
@@ -15,4 +17,13 @@ class SyntaxAnalyzerGenerator:
         terminal = line.split(' ')[1:]
         line = f.readline().strip()
         syn = line.split(' ')[1:]
+        self.syn.extend(syn)
         self.item_manager = ItemManager(terminal, nonterminal)
+        curr_symbol = ''
+        for line in f:
+            if line[0] != ' ':
+                curr_symbol = line.strip()
+                continue
+            else:
+                self.item_manager.add_production(curr_symbol, line.strip())
+
--- lab2/ItemManager.py
+++ lab2/ItemManager.py
@@ -1,8 +1,8 @@
 class ItemManager:
-    def __init__(self, nonterminal, terminal):
+    def __init__(self, terminal, nonterminal):
         self.grammar = dict()
         self.empty = set()
-        self.begins = dict()
+        self.begins = {'$':set()}
         self.nonterminal = set()
         self.terminal = set()
         self.first = None
@@ -10,13 +10,18 @@ class ItemManager:
         self.populate_nonterminal(nonterminal)
 
 
+    #item_tuple = (lijeva strana produkcije, desna strana produkcije kao string reprezentacija liste,
+    # set završnih simbola, index tockice)
+    # novo pocetno stanje: <Start_state_added>
+    # simbol za kraj niza: %EOF%
+
     def populate_terminal(self, terminal):
-        self.terminal.union(set(terminal))
+        self.terminal.update(set(terminal))
         for symbol in terminal:
             self.begins[symbol] = set([symbol])
 
     def populate_nonterminal(self, nonterminal):
-        self.nonterminal.union(set(nonterminal))
+        self.nonterminal.update(set(nonterminal))
         self.first = nonterminal[0]
         for symbol in nonterminal:
             self.begins[symbol] = set([symbol])
@@ -47,8 +52,21 @@ class ItemManager:
                             break
         return
 
-    def begins_single(self, series):
+    def begins_terminal(self, series):
         ret = set()
+        if series == ['$']:
+            return ret
+        for symbol in series:
+            ret.update(set(filter(lambda t: t in self.terminal, self.begins[symbol])))
+            if not self.is_empty(symbol):
+                break
+        return ret
+
+
+    def begins_shallow(self, series):
+        ret = set()
+        if series == ['$']:
+            return ret
         for i in range(len(series)):
             ret.add(series[i])
             if series[i] not in self.empty:
@@ -58,7 +76,7 @@ class ItemManager:
     def populate_begins_shallow(self):
         for production_set in self.grammar.items():
             for production in production_set[1]:
-                self.begins[production_set[0]].add(self.begins_single(production))
+                self.begins[production_set[0]].update(self.begins_shallow(production))
 
     def populate_begins(self):
         self.populate_begins_shallow()
@@ -66,28 +84,47 @@ class ItemManager:
         while changed:
             changed = False
             for symbol in self.nonterminal:
-                for start in self.begins[symbol]:
-                    if not self.begins[symbol].superset(self.begins[start]):
-                        self.begins[symbol].union(self.begins[start])
+                start_iteration = set(self.begins[symbol])
+                for start in start_iteration:
+                    if not self.begins[symbol].issuperset(self.begins[start]):
+                        self.begins[symbol].update(self.begins[start])
                         changed = True
 
-
-
-
-
-
-
-
+    # item_tuple = (lijeva strana produkcije, desna strana produkcije kao string reprezentacija liste,
+    # string reperezentacija seta završnih simbola, index tockice)
+    def transitions(self, item_tuple):
+        epsilon_transitions = []
+        transition = ()
+        curr_nonterminal = item_tuple[0]
+        production = eval(item_tuple[1])
+        curr_finish_set = eval(item_tuple[2])
+        curr_position = item_tuple[3]
+        if curr_position != len(production):
+            transition = (production[curr_position], (curr_nonterminal, repr(production), repr(curr_finish_set), curr_position+1))
+            if production[curr_position] in self.nonterminal:
+                next_finish_set = set()
+                for next_production in self.grammar[production[curr_position]]:
+                    if curr_position + 1 == len(production):
+                        next_finish_set.update(curr_finish_set)
+                    else:
+                        next_finish_set.update(self.begins_terminal(production[curr_position + 1:]))
+                        if self.is_empty(production[curr_position+1:]):
+                            next_finish_set.update(curr_finish_set)
+                    if next_production == ['$']:
+                        epsilon_transitions.append((production[curr_position], repr([]), repr(next_finish_set), 0))
+                    else:
+                        epsilon_transitions.append((production[curr_position], repr(next_production), repr(next_finish_set), 0))
+        return epsilon_transitions, transition
 
 
 
 if __name__ == '__main__':
-    im = ItemManager()
-    im.add_production("<A>", "$")
-    im.add_production("<A>", "b")
-    im.add_production("<B>", "<A>")
-    im.add_production("<B>", "<C> b")
-    im.add_production("<C>", "c")
+    im = ItemManager(['a','b'], ['<S>','<A>','<B>'])
+    im.add_production('<S>','<A>')
+    im.add_production('<A>','<B> <A>')
+    im.add_production('<A>','$')
+    im.add_production('<B>','a <B>')
+    im.add_production('<B>','b')
     im.populate_empty()
-    for elem in im.empty:
-        print(elem)
+    im.populate_begins()
+    exit(0)
