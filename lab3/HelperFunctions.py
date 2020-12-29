f39f187a432288bb58c892507d5f0869413899db
IvanKlabucar <ivan.klabucar@fer.hr>
2020-12-29 20:36:08 +0100

    comment in helper functions.py

--- lab3/HelperFunctions.py
+++ lab3/HelperFunctions.py
@@ -1,2 +1,2 @@
-def tilda(type1, type2):     # returns type1 ~ type2
+def tilda(type1, type2):     # returns type1 ~ type2, needs implementing, has to support calls like tilda('int', 'T')
     return True
